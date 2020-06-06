b05f10e5f5cc8f97e82cdb005a61f6b168b42006
FelipeBarros <barrosfelipe.013@gmail.com>
2020-06-06 00:17:38 -0300

    criando rotas

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+import logo from './images/enCurtaLogo.png';
+
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="background">
+        <div className="card">
+          <div className="logo">
+            <h1><i>EnCurta</i></h1>
+            <img src={logo} alt="logo" width="60" height="60" />
+          </div>
+
+          <Switch>
+            <Route exact path={"/"} component={Home} />
+            <Route exact path={"/login"} component={Login} />
+            <Route exact path={"/registrar"} component={Register} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
